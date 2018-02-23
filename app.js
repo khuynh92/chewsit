f8726e93f77407a5b796a83128f2f7fb5f1cf038
Khoa Huynh <khoawin92@gmail.com>
2018-02-23 00:34:08 -0800

    added conditionals for clicks

--- app.js
+++ app.js
@@ -22,21 +22,30 @@ new Restaurant('La Parisienne French Bakery', 'bakery', 'dessert', 'onedollar');
 new Restaurant('Storyville Coffee Company', 'cafe', 'snack', 'twodolla');
 
 function sumbitHandler() {
-  for (var i = 0; i < price.length; i++) {
-    if (price[i].checked) {
-      console.log(price[i].value + ' was clicked!');
-      price[i].checked = false;
-      break;
-    }
-  }
-  for (var j = 0; j < meal.length; j++) {
-    if (meal[j].checked) {
-      console.log(meal[j].value + ' was clicked!');
-      meal[j].checked = false;
-      break;
-    }
+  if (price[0].checked === false && price[1].checked === false && price[2].checked === false) {
+    alert('please choose a price range!');
   }
 
+  if (meal[0].checked === false && meal[1].checked === false && meal[2].checked === false && meal[3].checked === false & meal[4].checked === false) {
+    alert('please choose a meal type!');
+  }
+
+  if ((meal[0].checked === true || meal[1].checked === true || meal[2].checked === true || meal[3].checked === true || meal[4].checked === true) && (price[0].checked === true || price[1].checked === true || price[2].checked === true)) {
+    for (var i = 0; i < price.length; i++) {
+      if (price[i].checked) {
+        console.log(price[i].value + ' was clicked!');
+        price[i].checked = false;
+        break;
+      }
+    }
+    for (var j = 0; j < meal.length; j++) {
+      if (meal[j].checked) {
+        console.log(meal[j].value + ' was clicked!');
+        meal[j].checked = false;
+        break;
+      }
+    }
+  }
 }
 
 submit.addEventListener('click', sumbitHandler);
