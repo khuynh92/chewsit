ce9e246d846882934f9563779f876f839035a832
Khoa Huynh <khoawin92@gmail.com>
2018-02-23 00:24:54 -0800

    added functionality to radio buttons in JS

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 Restaurant.names = ['Green Leaf Vietnamese Restaurant', 'Zeeks Pizza', 'Tilikum Place Cafe', 'La Parisienne French Bakery', 'Storyville Coffee Company'];
 Restaurant.allRestaurants = [];
+var meal = document.getElementsByName('mealtype');
+var price = document.getElementsByName('dolla');
+var submit = document.getElementById('submit');
 
 //constructor function for Restaurants
 function Restaurant(name, cuisine, mealtype, price) {
@@ -10,8 +13,30 @@ function Restaurant(name, cuisine, mealtype, price) {
   this.mealtype = mealtype;
   this.price = price;
   Restaurant.allRestaurants.push(this);
-  
 }
 
 new Restaurant('Green Leaf Vietnamese Restaurant', 'southeast-asian', 'dinner', 'twodollars' );
-new Restaurant('Zeeks Pizza', 'italian', '');
+new Restaurant('Zeeks Pizza', 'italian', 'lunch', 'twodollar');
+new Restaurant('Tilikum Place Cafe', 'breakfast', 'breakfast', 'twodollars');
+new Restaurant('La Parisienne French Bakery', 'bakery', 'dessert', 'onedollar');
+new Restaurant('Storyville Coffee Company', 'cafe', 'snack', 'twodolla');
+
+function sumbitHandler() {
+  for (var i = 0; i < price.length; i++) {
+    if (price[i].checked) {
+      console.log(price[i].value + ' was clicked!');
+      price[i].checked = false;
+      break;
+    }
+  }
+  for (var j = 0; j < meal.length; j++) {
+    if (meal[j].checked) {
+      console.log(meal[j].value + ' was clicked!');
+      meal[j].checked = false;
+      break;
+    }
+  }
+
+}
+
+submit.addEventListener('click', sumbitHandler);
