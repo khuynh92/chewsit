9aeadb7b2b1f019f7ecd818a45a5dcc648f9af71
Khoa Huynh <khoawin92@gmail.com>
2018-02-23 01:26:51 -0800

    display choice as a li

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ function Restaurant(name, cuisine, mealtype, price) {
 
 //breakfast
 new Restaurant('Tilikum Place Cafe', 'breakfast', 'breakfast', 'twodollars');
-new Restaurant('Breakfast2', 'any', 'breakfast', 'onedollar');
+new Restaurant('Breakfast1.1', 'any', 'breakfast', 'onedollar');
+new Restaurant('Breakfast1.2', 'any', 'breakfast', 'onedollar');
+new Restaurant('Breakfast1.3', 'any', 'breakfast', 'onedollar');
+new Restaurant('Breakfast1.4', 'any', 'breakfast', 'onedollar');
 new Restaurant('Breakfast3', 'any', 'breakfast', 'threedollars');
 //lunch
 new Restaurant('Zeeks Pizza', 'italian', 'lunch', 'twodollars');
@@ -40,16 +43,17 @@ new Restaurant('dessert3', 'any', 'dessert', 'threedollars');
 
 function sumbitHandler() {
   var results = [];
-
+  //check if user selected price range
   if (price[0].checked === false && price[1].checked === false && price[2].checked === false) {
     alert('please choose a price range!');
   }
-
+  //check if user selected mealtype
   if (meal[0].checked === false && meal[1].checked === false && meal[2].checked === false && meal[3].checked === false & meal[4].checked === false) {
     alert('please choose a meal type!');
   }
-
+  // run meal matcher when both price and mealtype are selected
   if ((meal[0].checked === true || meal[1].checked === true || meal[2].checked === true || meal[3].checked === true || meal[4].checked === true) && (price[0].checked === true || price[1].checked === true || price[2].checked === true)) {
+  //assign user price choice to a variable
     for (var i = 0; i < price.length; i++) {
       if (price[i].checked) {
         console.log(price[i].value + ' was clicked!');
@@ -58,6 +62,7 @@ function sumbitHandler() {
         break;
       }
     }
+    //assign user mealtype choice to a variable
     for (var j = 0; j < meal.length; j++) {
       if (meal[j].checked) {
         console.log(meal[j].value + ' was clicked!');
@@ -66,13 +71,23 @@ function sumbitHandler() {
         break;
       }
     }
+    //look through all restaurants that match users choice, and store them in an array
     for (var k = 0; k < Restaurant.allRestaurants.length; k++) {
       if (Restaurant.allRestaurants[k].mealtype === mainMeal && Restaurant.allRestaurants[k].price === mainPrice) {
         results.push(Restaurant.allRestaurants[k]);
         console.log(results);
       }
     }
+    //create a random number using the array
+    var ranNumber = Math.floor(Math.random()*results.length);
+    console.log(ranNumber);
+    //display random result
+    var testEl = document.getElementById('test');
+    var liEl = document.createElement('li');
+    liEl.appendChild(document.createTextNode('You\'re eating at ' + results[ranNumber].name));
+    testEl.appendChild(liEl);
   }
+  //Create button/logic for multiple tries
 }
 
 submit.addEventListener('click', sumbitHandler);
