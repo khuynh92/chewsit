915ea10027d3119ee882dd73891260d3b7812b2a
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-02-24 09:49:27 -0800

    added some js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 var userDb = [];
 var createAccount = document.getElementById('createButton');
 var signIn = document.getElementById('signInButton');
+var signInButtonPopup = document.getElementById('signInButtonPopup');
 var popUp = document.getElementById('myModal');
+var span = document.getElementsByClassName('close')[0];
 
 function Constructor(userName, userCity, userCnum) {
   this.name = userName;
@@ -18,7 +20,6 @@ function handleContactSubmit(event) {
   var userName = document.getElementById('userName').value;
   var userCity = document.getElementById('userCity').value;
   var userCnum = parseInt(document.getElementById('userCnum').value);
-  var span = document.getElementsByClassName("close")[0];
 
   //   var userName = event.target.userName.value;
   //   var userCity = event.target.userCity.value;
@@ -37,12 +38,23 @@ function handleSignIn(event) {
   popUp.style.display = 'block';
   console.log('sign in button pushed');
 }
+function handleSignInPopup(event) {
+  event.preventDefault();
+  popUp.style.display = 'block';
+  console.log('sign in button pushed');
 
+}
+span.onclick = function() {
+  popUp.style.display = "none";
+};
 window.onclick = function(event) {
   if (event.target === popUp) {
     popUp.style.display = 'none';
   }
 };
+if (signInButtonPopup) {
+  signInButtonPopup.addEventListener('click', handleSignInPopup);
+}
 if (signIn) {
   signIn.addEventListener('click', handleSignIn);
 }
